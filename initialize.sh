fc78540288dfabce106ab6311006ca7fa866297d
Vitaliy Vorobyov <vitalik.w555@gmail.com>
2022-09-29 23:44:06 +0300

    Fix some issues with the colab

--- initialize.sh
+++ initialize.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 pip install -r requirements.txt
-git clone http://gitlab.aicrowd.com/aicrowd/challenges/iglu-challenge-2022/iglu-2022-rl-mhb-baseline.git
+
+if [ "$1" = "--colab" ]; then
+    git lfs clone http://gitlab.aicrowd.com/aicrowd/challenges/iglu-challenge-2022/iglu-2022-rl-mhb-baseline.git
+else
+  git lfs clone http://gitlab.aicrowd.com/aicrowd/challenges/iglu-challenge-2022/iglu-2022-rl-mhb-baseline.git
+fi
+
 cp -r ./iglu-2022-rl-mhb-baseline/agents/mhb_baseline/nlp_model/model ./nlp_model/
 cp -r ./iglu-2022-rl-mhb-baseline/agents/mhb_baseline/nlp_model/tokenizer ./nlp_model/
 cp ./iglu-2022-rl-mhb-baseline/agents/mhb_baseline/nlp_model/t5-autoregressive-history-3-best.pt ./nlp_model/
